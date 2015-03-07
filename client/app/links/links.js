4df924ee3811be009fe13f9a145ef11e787d6472
yesplease <bradley.heinz@gmail.com>
2015-03-07 10:46:08 -0800

    Add: shorten link Fix: return of link controller

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,14 +1,16 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, $window, Links) {
+  $scope.data = {};
   $scope.dummyLink = 'now it\'s working woo hoo!!';
   $scope.getLinks = function(){
+    // console.log(Links.getLinks());
     Links.getLinks()
       .then(function(links){
-        $scope.data = links;
-        console.log("scope.data", $scope.data);
+        $scope.data.links = links.data;
     })
   }
+  // console.log('scope.getLinks:', $scope.getLinks())
 $scope.getLinks();
 });
 
